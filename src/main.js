cb7596a68ebc6a53a2586ff6823ab070fafb3898
rhdmswls12 <rhgusdnr1293@naver.com>
2023-08-04 16:00:45 +0900

    Create project

---
+++ src/main.js
@@ -0,0 +1,8 @@
+// main.js
+import App from "./App";
+import router from './routes'; //index.js 가져오기
+
+const root = document.querySelector('#root')
+root.append(new App().el)
+
+router()
---
+++ src/store/about.js
@@ -0,0 +1,10 @@
+// about.js
+import { Store } from "../core/core";
+
+export default new Store({
+  photo: 'https://avatars.githubusercontent.com/u/71330240?v=4',
+  name: 'rhdmswls12 / Eunjin',
+  email: 'a66613075@gmail.com',
+  github: 'https://github.com/rhdmswls12',
+  repository: 'https://github.com/rhdmswls12/vanillajs-movie-app'
+})
